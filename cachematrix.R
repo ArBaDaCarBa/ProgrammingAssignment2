8d936bed38f09a2275f2559813d3bc7200d00317
Bertrand Frottier <bertrand.frottier@gmail.com>
2015-01-17 00:03:42 +0100

    Documentation for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of functions to create special matrix object, able to cache its
+## inverted value and avoid costly computation
 
-## Write a short comment describing this function
-
+## creates a special matrix that can cache it's inverted matrix
+##
+## The function will return four function to manipulate the matrix and it's
+## inverted matrix:
+##
+## $get returns the original matrix
+## $getinverted returns the cached inverted matrix
+## $set sets the original matrix and empties the cache
+## $setinverted sets the inverted matrix in cache
+##
+## Use cacheSolve for easy use of the structure
+##
+## !!! Does not check is the matrix is inversible !!!
 makeCacheMatrix <- function(x = matrix()) {
 
 }
