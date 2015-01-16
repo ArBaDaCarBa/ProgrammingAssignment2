ea1fe17532cf8fc2f917e7a3f051469068c83d15
Bertrand Frottier <bertrand.frottier@gmail.com>
2015-01-16 23:40:27 +0100

    Creating runit test file

---
+++ runit.cachematrix.R
@@ -0,0 +1,22 @@
+test.makeCacheMatrix <- function() {
+    # This is the original matrix and the expected result
+    ori_m <- matrix(c(7, 3, -2, 5), nrow=2)
+    inv_m <- solve(m)
+
+    # Let's create the cache matrix, cache should be empty
+    m <- makeCacheMatrix(ori_m)
+    checkTrue(is.null(m$getinverted()))
+
+    # And when we compute the inverted matrix, the cache should be set
+    # and the values correct
+    inv <- cacheSolve(m)
+    checkEquals(inv, inv_m)
+    checkTrue(!is.null(m$getinverted()))
+    checkEquals(m$getinverted(), inv_m)
+
+    # Even twice, just to be sure
+    inv <- cacheSolve(m)
+    checkEquals(inv, inv_m)
+    checkTrue(!is.null(m$getinverted()))
+    checkEquals(m$getinverted(), inv_m)
+}
